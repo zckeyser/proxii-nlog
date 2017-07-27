7869bd2aa220650d86a2560061b31321859761e5
zckeyser <zckeyser@gmail.com>
2017-07-27 01:32:11 -0400

    added LogException and LogCallsObject implementations

--- Proxii.NLog/LogCallsObjectExtensions.cs
+++ Proxii.NLog/LogCallsObjectExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace Proxii.NLog
 {
@@ -11,19 +13,62 @@ namespace Proxii.NLog
         /// <summary>
         /// Logs a JSON object on interception in varying detail levels (https://github.com/zckeyser/proxii-nlog/wiki/LogCallsObject)
         /// </summary>
-        public static IProxii<T> LogCallsObject<T>(this IProxii<T> proxii, LogDetailLevel detailLevel)
+        public static IProxii<T> LogCallsObject<T>(this IProxii<T> proxii, LogLevel logLevel, LogDetailLevel detailLevel = LogDetailLevel.Low)
             where T : class
         {
-
-
-            return proxii;
+            return proxii.BeforeInvoke(CreateLogCallsObjectAction(typeof(T).GetLogger(), logLevel, detailLevel));
         }
 
-        private static Action<MethodInfo, object[]> CreateLogCallsObjectAction(Logger logger, LogLevel logLevel, string format)
+        /// <summary>
+        /// Logs a JSON object on interception in varying detail levels (https://github.com/zckeyser/proxii-nlog/wiki/LogCallsObject)
+        /// </summary>
+        public static IProxii<T> LogCallsObject<T>(this IProxii<T> proxii, LogDetailLevel detailLevel = LogDetailLevel.Low)
+            where T : class
+        {
+            return proxii.LogCallsObject(LogLevel.Info, detailLevel);
+        }
+
+        private static Action<MethodInfo, object[]> CreateLogCallsObjectAction(Logger logger, LogLevel logLevel, LogDetailLevel detailLevel)
         {
             return (method, args) =>
             {
-                logger.Log(logLevel, format, method.GetMethodSignature(), string.Join(", ", args));
+                switch (detailLevel)
+                {
+                    case LogDetailLevel.Low:
+                        logger.Log(logLevel, GetLowDetailLog(method));
+                        break;
+                    case LogDetailLevel.Medium:
+                        logger.Log(logLevel, GetMediumDetailLog(method, args));
+                        break;
+                    case LogDetailLevel.High:
+                        // i don't think this is actually possible to make
+                        break;
+                    default:
+                        break;
+                }
+                
+            };
+        }
+
+        private static LowDetailLog GetLowDetailLog(MethodInfo method)
+        {
+            return new LowDetailLog
+            {
+                timestamp = DateTime.Now.ToString("O"),
+                className = method.DeclaringType.FullName,
+                methodName = method.Name
+            };
+        }
+
+        private static MediumDetailLog GetMediumDetailLog(MethodInfo method, object[] args)
+        {
+            return new MediumDetailLog
+            {
+                timestamp = DateTime.Now.ToString("O"),
+                className = method.DeclaringType.FullName,
+                methodName = method.Name,
+                methodSignature = method.GetMethodSignature(),
+                arguments = args
             };
         }
     }
@@ -33,5 +78,31 @@ namespace Proxii.NLog
         Low,
         Medium,
         High
+    }
+
+    public class LowDetailLog
+    {
+        public string methodName;
+        public string timestamp;
+        public string className;
+    }
+
+    public class MediumDetailLog
+    {
+        public string methodName;
+        public string timestamp;
+        public string className;
+        public string methodSignature;
+        public object[] arguments;
+    }
+
+    public class HighDetailLog
+    {
+        public string methodName;
+        public string timestamp;
+        public string className;
+        public string methodSignature;
+        public string[] arguments;
+        public object callingObject;
     }
 }
---
+++ Proxii.NLog/LogExceptionsExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+using NLog;
+
+namespace Proxii.NLog
+{
+    public static class LogExceptionsExtension
+    {
+        public static IProxii<T> LogExceptions<T>(this IProxii<T> proxii, LogLevel logLevel)
+            where T : class
+        {
+            return proxii.Catch<Exception>(LogExceptionsAction(typeof(T).GetLogger(), logLevel));
+        }
+
+        public static IProxii<T> LogExceptions<T>(this IProxii<T> proxii)
+            where T : class
+        {
+            return proxii.LogExceptions(LogLevel.Error);
+        }
+
+        private static Action<Exception> LogExceptionsAction(Logger logger, LogLevel logLevel)
+        {
+            return e =>
+            {
+                logger.Log(logLevel, JsonConvert.SerializeObject(e));
+            };
+        }
+    }
+}
