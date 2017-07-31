553abf0a73599cf1f521ea4ab3d1eb7f187a41f8
zckeyser <zckeyser@gmail.com>
2017-07-30 20:14:28 -0400

    first LogCallsObject test

---
+++ Proxii.NLog.Test/LogCallsObjectTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Proxii.NLog.Test
+{
+    [TestFixture]
+    public class LogCallsObjectTest : BaseTest
+    {
+        const string ISODateTimePattern = "\"\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d+[+-]\\d{2}:\\d{2}\"";
+
+        [Test]
+        public void LogCallsObject_LowDetail()
+        {
+            var proxy = Proxii.Proxy<ILogTester, LogTester>()
+                              .LogCallsObject(LogDetailLevel.Low)
+                              .Create();
+
+            proxy.Do();
+
+            string pattern = $"{{\"methodName\":\"Do\",\"timestamp\":{ISODateTimePattern},\"className\":\"Proxii\\.NLog\\.Test\\.ILogTester\"}}";
+
+            Assert.That(Regex.IsMatch(InfoLogContents[0], pattern), Is.True);
+        }
+
+        [Test]
+        public void LogCallsObject_MediumDetail()
+        {
+
+        }
+
+        [Test]
+        public void LogCallsObject_DefaultsToLowDetail()
+        {
+
+        }
+
+        [Test]
+        public void LogCallsObject_UsesLogLevel()
+        {
+
+        }
+    }
+}
